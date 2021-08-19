13b8d9401dc1ced18cf00d0fefb620aecdbe35fd
Ayrton de Freitas Silva Diniz <89053790+AyrtonFSDiniz@users.noreply.github.com>
2021-08-19 02:05:40 +0000

    Initial commit

---
+++ String.js
@@ -0,0 +1,14 @@
+// Strings --> texto
+
+//let nome = "Ayrton";
+//console.log(nome);
+
+//let idade = '27';
+//console.log(idade);
+
+let mensagem1 = "Hi";
+let mensagem2 = "How are you?";
+//Dá pra fazer das duas formas como abaixo
+//let mensagemfinish = mensagem1+" , tudo bem "+mensagem2;
+//console.log(mensagemfinish);
+//console.log(mensagem1+" , tudo bem "+mensagem2);
---
+++ index.js
@@ -0,0 +1,10 @@
+//Explicação sobre concarternar e tambem sobre usar o acento agudo para usar quando tiver que repetir informação varias vezes.
+
+//let titulo = "Programação";
+//let linguagem = " em JavaScript";
+//console.log(titulo+linguagem);
+//let ling = titulo + linguagem;
+//console.log(ling);
+
+let mensagem = "Oiioiioioioioioioioioioioioioioiioioioioioioioioioioioioioioioioioioioioioioioioioioioioioioioioioioioioioioioioioioioioioi";
+console.log(`Quantos caracteres tem acima? Tem ${mensagem.length} caracteres`);
